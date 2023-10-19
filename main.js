fda9adbafec2398b82822419d8add1002ba441a5
Wexlersolk~ <danylkarp@gmail.com>
2023-10-19 10:08:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const xml = require("fast-xml-parser");
+const fs = require("node:fs");
+const port = 8000;
+
+
+const parser = new xml.XMLParser();
+const builder = new xml.XMLBuilder({ format: true });
+
+const server = http.createServer((req, res) => {
+  const xmldata = fs.readFileSync("data.xml", "utf-8");
+  const obj = parser.parse(xmldata);
+
+  const necessaryData = Array.isArray(obj.exchange.currency)
+  ? obj.exchange.currency.map(item => ({
+      date: item.exchangedate,
+      rate: item.rate
+    }))
+  : ["not an array"];
+
+
+  const xmlObj = {
+    data: {
+      exchange: necessaryData
+    }
+  };
+
+  const ActualXml = builder.build(xmlObj);
+  res.setHeader('Content-Type', 'application/xml');
+  res.end(ActualXml);
+});
+
+
+server.listen(port, () => {
+  console.log(`Works on port ${port}`);
+});
